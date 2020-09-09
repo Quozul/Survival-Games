a1cb9b05799bfe262876c4201db6f9bab54c15a0
Quozul <30729291+Quozul@users.noreply.github.com>
2020-09-09 19:25:09 +0200

    Fixes

--- src/dev/quozul/UHC/Game.java
+++ src/dev/quozul/UHC/Game.java
@@ -21,6 +21,7 @@ public class Game implements CommandExecutor {
 
     // Constants
 
+    public static NamespacedKey gameProgressBossBarNamespace = new NamespacedKey(Main.plugin, "uhc_progress");
     private double circle = Math.PI * 2;
 
     // Global game variables
@@ -34,29 +35,23 @@ public class Game implements CommandExecutor {
     // Game parameters
 
     // TODO: Put this in config file
-    public static int durationMinutes = 10; // Duration of the UHC game in minutes
-    public static int durationTicks = durationMinutes * 60 * 20;
+    public static int durationMinutes = 20; // Duration of the UHC game in minutes
     // Recommended values: 128, 256, 512, 1024, 2048
-    public static int startSize = 1024; // Diameter of the border on start
-    public static int steps = 1; // Border decrease steps
-    public static int borderDecreaseSpeed = durationMinutes * 60; // Time for the border to reach the next step in seconds
+    public static int startSize = 2048; // Diameter of the border on start
     public static int maxDamage = 10; // Max world border damage
+
     // Create default teams
     public static Map<String, ChatColor> teamNames = new HashMap<>();
 
     // Calculated parameters
 
-    public static int stepSize = durationTicks / steps;
-    public static int borderDecreaseSize = startSize / steps;
-    private int a = steps;
-
-    // Constants
-    public static NamespacedKey gameProgressBossBarNamespace = new NamespacedKey(Main.plugin, "uhc_progress");
+    public static int durationTicks = durationMinutes * 60 * 20;
 
     public static List<World> getUHCWorlds() {
         List<World> worlds = new ArrayList<>();
         worlds.add(Bukkit.getServer().getWorld(Game.worldName));
         worlds.add(Bukkit.getServer().getWorld(Game.worldName + "_nether"));
+
         return worlds;
     }
 
@@ -78,6 +73,7 @@ public class Game implements CommandExecutor {
 
             // Reset time
             world.setFullTime(0);
+            world.setStorm(false);
 
             // Set gamerules
             world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, true);
@@ -86,7 +82,7 @@ public class Game implements CommandExecutor {
         }
 
         // Create the step progress boss bar
-        BossBar bossBar = Bukkit.getServer().createBossBar(gameProgressBossBarNamespace, "Test", BarColor.YELLOW, BarStyle.SEGMENTED_10);
+        BossBar bossBar = Bukkit.getServer().createBossBar(gameProgressBossBarNamespace, "Bordure", BarColor.RED, BarStyle.SEGMENTED_10);
         bossBar.setVisible(true);
         bossBar.setProgress(0);
 
@@ -100,32 +96,29 @@ public class Game implements CommandExecutor {
             if (team.getSize() > 0) filledTeams++;
 
         double radiusBetweenTeams = circle / filledTeams;
-
         double salt = Math.random() * circle;
+        int i = 0;
 
         for (Team team : teams) {
-            int teamIndex = teams.indexOf(team);
+            if (team.getSize() > 0) {
+                i++;
 
-            for (String entry : team.getEntries()) {
-                Player player = Bukkit.getPlayer(entry);
+                for (String entry : team.getEntries()) {
+                    Player player = Bukkit.getPlayer(entry);
 
-                if (player != null) {
+                    if (player != null) {
+                        // Teleports every players with their team to a equal distance from each others
+                        int x = (int) Math.round(Math.cos(i * radiusBetweenTeams + salt) * (startSize / 2.5));
+                        int z = (int) Math.round(Math.sin(i * radiusBetweenTeams + salt) * (startSize / 2.5));
 
-                    // Teleports every players with their team to a equal distance from each others
-                    int x = (int) Math.round(Math.cos(teamIndex * radiusBetweenTeams + salt) * (startSize / 2.5));
-                    int z = (int) Math.round(Math.sin(teamIndex * radiusBetweenTeams + salt) * (startSize / 2.5));
+                        World world = Bukkit.getWorld(worldName);
+                        Location loc = new Location(world, x, 255, z);
 
-                    World world = Bukkit.getWorld(worldName);
-                    Location loc = new Location(world, x, 255, z);
+                        player.teleport(loc);
 
-                    player.teleport(loc);
-
-                    // Add potion effects
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 999999, 0));
-
-                    player.addScoreboardTag("spawning");
-                    player.addScoreboardTag("playing");
-
+                        // Add potion effects
+                        player.addPotionEffect(new PotionEffect(PotionEffectType.SLOW_FALLING, 999999, 0));
+                    }
                 }
             }
         }
@@ -158,12 +151,6 @@ public class Game implements CommandExecutor {
             else
                 infoObjective = board.registerNewObjective("UHCInfo", "dummy", "§6§lUHC");
 
-            /*Score step = infoObjective.getScore("§7» Etape");
-            step.setScore(0);
-            Score nextBorderRadius = infoObjective.getScore("§7» Prochain rayon");
-            nextBorderRadius.setScore(0);
-            Score nextBorder = infoObjective.getScore("§7» Prochaine zone (s)");
-            nextBorder.setScore(0);*/
             Score borderRadius = infoObjective.getScore("§7» Rayon");
             borderRadius.setScore(0);
 
@@ -182,6 +169,11 @@ public class Game implements CommandExecutor {
 
             // Set player's gamemode
             player.setGameMode(GameMode.SURVIVAL);
+
+            // Set player's tags
+            player.addScoreboardTag("spawning");
+            player.addScoreboardTag("playing");
+            player.removeScoreboardTag("died");
         }
 
     }
@@ -213,6 +205,8 @@ public class Game implements CommandExecutor {
                 winner = player;
 
             player.getScoreboardTags().remove("playing");
+
+            player.getInventory().clear();
         }
 
         if (winner != null)
@@ -235,18 +229,6 @@ public class Game implements CommandExecutor {
         }, 200);
     }
 
-    public int borderDiameter(int step) {
-        double borderPercentage = step * borderDecreaseSize / (double)startSize;
-        double borderPercentageExponential = ((Math.pow(a, (1 - borderPercentage)) - 1) / (a - 1));
-        int currentBorder = (int) Math.floor(startSize * borderPercentageExponential);
-
-        return currentBorder;
-    }
-
-    public int getStep() {
-        return (int) Math.floor(gameTime / (float)stepSize);
-    }
-
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if (!commandSender.isOp())
@@ -258,50 +240,15 @@ public class Game implements CommandExecutor {
 
             gameTime += interval;
 
-            int currentStep = getStep();
-            int currentBorder = borderDiameter(currentStep);
-            int nextBorderDiameter = borderDiameter(currentStep + 1);
-            int damageAmount = currentStep / steps * (maxDamage - 1) + 1;
-            boolean newStep = gameTime % stepSize == 0;
-            int nextStepCooldown = (currentStep + 1) * stepSize - gameTime; // Next step in ticks
-
-            if (newStep) {
-                // World border
-                /*List<World> worlds = getUHCWorlds();
-
-                for (World world : worlds) {
-                    WorldBorder worldBorder = world.getWorldBorder();
-
-                    worldBorder.setSize(currentBorder, borderDecreaseSpeed);
-                    worldBorder.setDamageAmount(damageAmount);
-                }*/
-
-                BossBar bossBar = Bukkit.getServer().getBossBar(new NamespacedKey(Main.plugin, "uhc_progress"));
-                bossBar.setProgress(currentStep / (float)steps);
-
-                // TODO: Play sound and display title on new step/border resize
-                for (Player player : Bukkit.getServer().getOnlinePlayers()) {
-                    player.sendTitle("§4Réduction de la bordure", "§7Diamètre : §8" + currentBorder, 10, 40, 10);
-                    player.playSound(player.getLocation(), Sound.ENTITY_ENDER_DRAGON_GROWL, 1, 1);
-                }
-
-                Bukkit.broadcastMessage("§4Réduction de la bordure. §7Diamètre : §8" + currentBorder);
-            }
+            double bossBarPercentage = Bukkit.getServer().getWorld(worldName).getWorldBorder().getSize() / (float)startSize;
+            BossBar bossBar = Bukkit.getServer().getBossBar(gameProgressBossBarNamespace);
+            bossBar.setProgress(bossBarPercentage);
 
             for (Player player : Bukkit.getServer().getOnlinePlayers()) {
                 // Update scoreboard
                 Scoreboard board = player.getScoreboard();
 
                 Objective obj = board.getObjective("UHCInfo");
-
-                /*Score step = obj.getScore("§7» Etape");
-                step.setScore(currentStep);
-
-                Score nextBorder = obj.getScore("§7» Prochaine zone (s)");
-                nextBorder.setScore(nextStepCooldown / 20);
-
-                Score nextRadius = obj.getScore("§7» Prochain rayon");
-                nextRadius.setScore(nextBorderDiameter / 2);*/
 
                 int currentBorderRadius = (int) Math.floor(player.getWorld().getWorldBorder().getSize() / 2);
                 Score borderRadius = obj.getScore("§7» Rayon");
@@ -310,7 +257,7 @@ public class Game implements CommandExecutor {
                 player.setScoreboard(board);
             }
 
-            if (currentStep >= steps)
+            if (gameTime >= durationTicks)
                 if (evaluateUHC()) {
                     Bukkit.getServer().getScheduler().cancelTask(task);
                     finishUHC();
